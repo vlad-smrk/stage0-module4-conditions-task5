f154df26cbf6c25e76bf17c06badb460f3a3b31b
Vladislav Samorukov <smrk.vld@gmail.com>
2023-02-15 01:27:55 +0300

    done2

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,6 +2,6 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
-        System.out.println(year % 4 == 0 ? "leap" : "not leap");
+        System.out.println(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) ? "leap" : "not leap");
     }
 }
